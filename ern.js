531effb383b4948b3758065edfc719f4a420153b
James R Brown <generaldelta@yahoo.com>
2019-02-26 16:52:32 -0700

    Refactored code to make things more granular.

--- ern.js
+++ ern.js
@@ -14,9 +14,7 @@
     });//*/ 
     
     const fs = require('fs');
-    const TVDB = require('node-tvdb');
-    //API documentation: https://edwellbrook.github.io/node-tvdb/Client.html#getSeriesById
-    const tvdb = new TVDB('4296B7GBEUY13UII');
+
     
     var dir = "/mnt/stronghold/data/video/Animation/Series/Invader\ Zim";
     var searchTerm = "Invader ZIM";
@@ -33,9 +31,31 @@
             return `S${pad(seasonNumber, amount)}E${pad(episodeNumber, amount)}`;
         };
         
+        function matchRating(text, searchTerm){
+            var matched = 0;
+            
+            if(text.toUpperCase().includes(searchTerm.toUpperCase())){
+                matched = 1;
+            }else{
+                var terms = searchTerm.match(/\w+/g);
+                        
+                var matches = 0;
+
+                for(var i = 0, l = terms.length; i < l; i++){
+                    if(text.toUpperCase().includes(terms[i].toUpperCase())){
+                        matches++;
+                    }
+                }
+                matched = matches / terms.length;
+            }
+            
+            return matched;
+        };
+        
         return {
             pad: pad,
-            standardSequenceID: standardSequenceID
+            standardSequenceID: standardSequenceID,
+            matchRating: matchRating
         };
         
     }());
@@ -46,7 +66,7 @@
         
         function filterDirectories(files){
             var file = files.shift();
-            for(;file;){
+            while(file){
                 
                 if(!fs.lstatSync(`${dir}/${file}`).isDirectory()){
                     listOfFiles.push(file);
@@ -64,92 +84,141 @@
         
     };
     
+    var api = (function(){
+        const TVDB = require('node-tvdb');
+        //API documentation: https://edwellbrook.github.io/node-tvdb/Client.html#getSeriesById
+        const tvdb = new TVDB('4296B7GBEUY13UII');
+        
+        var get = {
+            seriesByID : function(id, callback){
+                function processReturn(response){
+
+                };
+
+                tvdb.getSeriesAllById(id)
+                .then(response => { 
+                    callback(response);
+                })
+                .catch(error => { 
+                    log.error(error);  
+                    callback({episodes: []});
+                });
+            }
+        };
+        
+        function getMetaData(searchTerm, callback){
+            log.log(searchTerm);
+            function searchForSeries(searchTerm, callback){
+                log.log(searchTerm);
+                var metaData = [];
+                tvdb.getSeriesByName(searchTerm)
+                .then(response => { 
+                    //log.log(response);
+                    for(var i = 0, l = response.length; i < l; i++){
+                        metaData.push({
+                            seriesName: response[i].seriesName,
+                            id: response[i].id,
+                            slug: response[i].slug,
+                            firstAired: response[i].firstAired
+                        });
+                    }
+                    log.log(metaData);
+                    callback(metaData);
+                })
+                .catch(error => { log.error(error);  });
+            };
+
+            function getSeriesById(id, callback){
+                function processReturn(response){
+                    
+                };
+                
+                tvdb.getSeriesAllById(id)
+                .then(response => { 
+                    callback(response);
+                })
+                .catch(error => { 
+                    log.error(error);  
+                    callback({episodes: []});
+                });
+            };
+
+            function main(searchTerm, callback){
+                var metaData = [];
+
+                function processSearchResults(results, callback){
+                    var result = results.shift();
+                    log.log(result);
+                    if(result && result.id){
+                        result.episodes = [];
+                        getSeriesById(result.id, function(response){
+                            var episode = response.episodes.shift();
+                            while(episode){
+                                result.episodes.push({
+                                    id: episode.id,
+                                    airedSeason: episode.airedSeason,
+                                    airedEpisodeNumber: episode.airedEpisodeNumber,
+                                    episodeName: episode.episodeName,
+                                    standardSequenceID: tools.standardSequenceID(episode.airedSeason, episode.airedEpisodeNumber)
+                                });
+
+                                episode = response.episodes.shift();
+                            }
+                            metaData.push(result);
+                            processSearchResults(results, callback);
+                        });
+                    }else{
+                        callback();
+                    }
+                };
+
+                searchForSeries(searchTerm, function(results){
+                    //log.log(results);
+                    processSearchResults(results, function(){
+                        callback(metaData);
+                    });
+                });
+            }
+
+            main(searchTerm, function(metaData){
+                callback(metaData);
+            });
+        };
+        
+        return {
+            get:{
+                seriesByID: get.seriesByID
+            }
+        };
+        
+    })();
     
     //search: 'Invader ZIM'
-    function getMetaData(searchTerm, callback){
-        log.log(searchTerm);
-        function searchForSeries(searchTerm, callback){
-            log.log(searchTerm);
-            var metaData = [];
-            tvdb.getSeriesByName(searchTerm)
-            .then(response => { 
-                //log.log(response);
-                for(var i = 0, l = response.length; i < l; i++){
-                    metaData.push({
-                        seriesName: response[i].seriesName,
-                        id: response[i].id,
-                        slug: response[i].slug,
-                        firstAired: response[i].firstAired
-                    });
-                }
-                log.log(metaData);
-                callback(metaData);
-            })
-            .catch(error => { log.error(error);  });
-        };
-        
-        function getSeriesById(id, callback){
-            tvdb.getSeriesAllById(id)
-            .then(response => { 
-                callback(response);
-            })
-            .catch(error => { 
-                log.error(error);  
-                callback({episodes: []});
-            });
-        };
-            
-        function main(searchTerm, callback){
-            var metaData = [];
-            
-            function processSearchResults(results, callback){
-                var result = results.shift();
-                log.log(result);
-                if(result && result.id){
-                    result.episodes = [];
-                    getSeriesById(result.id, function(response){
-                        var episode = response.episodes.shift();
-                        for(;episode;){
-                            result.episodes.push({
-                                id: episode.id,
-                                airedSeason: episode.airedSeason,
-                                airedEpisodeNumber: episode.airedEpisodeNumber,
-                                episodeName: episode.episodeName,
-                                standardSequenceID: tools.standardSequenceID(episode.airedSeason, episode.airedEpisodeNumber)
-                            });
-                            
-                            episode = response.episodes.shift();
-                        }
-                        metaData.push(result);
-                        processSearchResults(results, callback);
-                    });
-                }else{
-                    callback();
-                }
-            };
-            
-            searchForSeries(searchTerm, function(results){
-                //log.log(results);
-                processSearchResults(results, function(){
-                    callback(metaData);
-                });
-            });
-        }
-        
-        main(searchTerm, function(metaData){
-            callback(metaData);
-        });
-    };
     
     
-    //*
+    
+    /*
     getMetaData(searchTerm, function(metaData){
         log.log(JSON.stringify(metaData));
     });//*/
+    
+    api.get.seriesByID(75545, function(response){
+        console.log(response);
+    });
     
     /*
     getListOfFiles(dir, function(result){
         log.log(result);
     });//*/
+    
+    function mapper(fileNames, metaData){
+        var IDs = [];
+        var files = {};
+        
+        
+        
+        
+       
+    };
     
 })();
